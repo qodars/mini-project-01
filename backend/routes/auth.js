318b39c071c249d4cd7d42a307931666504d02fe
Rinaldy97 <rinaldysaputra77@gmail.com>
2023-04-14 18:21:52 +0700

    commit

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,4 +1,11 @@
+const {authController, categoryControll} = require("../controller");
 const router = require("express").Router();
 
+
+router.post("/register", authController.register);
+
+//category
+router.post("/tambah/category", categoryControll.insert);
+router.patch("/ubah/category/:category_id?", categoryControll.update)
 
 module.exports = router;
