d2219f39a84346ca063ec0987b8f1d27ad9e495f
Rinaldy97 <rinaldysaputra77@gmail.com>
2023-05-07 22:32:33 +0700

    push

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const port = 8000;
-
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
-app.use(express.json());
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// app.use(express.json());
 
 
 //db
